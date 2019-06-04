c579e31fabf5061c5e9bd85a4fea979dfa4d2222
urdans <urdans@gmail.com>
2019-06-04 19:31:56 -0400

    Setting up Git hub

---
+++ src/main/java/com/urdans/eetools/EetoolsApplication.java
@@ -0,0 +1,18 @@
+package com.urdans.eetools;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+
+@SpringBootApplication
+public class EetoolsApplication {
+
+	public static void main(String[] args) {
+		run(EetoolsApplication.class, args);
+//		SpringApplication.run(EetoolsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/urdans/eetools/controllers/MainController.java
@@ -0,0 +1,18 @@
+package com.urdans.eetools.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.RequestWrapper;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public String index(Model model){
+		return "index/index";
+	}
+}
